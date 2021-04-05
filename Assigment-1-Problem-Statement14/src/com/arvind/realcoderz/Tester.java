16973a322cdde8576ea5f1517851141b86c2e32e
arvindtl <Arvind@DESKTOP-4NONCLR>
2021-04-05 13:35:17 -0700

    commit by arvind

---
+++ Assigment-1-Problem-Statement14/src/com/arvind/realcoderz/Tester.java
@@ -0,0 +1,15 @@
+package com.arvind.realcoderz;
+
+public class Tester {
+	public static void main(String[] args) {
+		do {
+			System.out.println("do inside");
+		} while (false);
+		
+		System.out.println();
+		while (true) {
+			System.out.println();
+		}
+		//System.out.println("outside loop");
+	}
+}
