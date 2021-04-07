02a9a3621cf0a6442361bd6dffba809139b45617
arvindtl <Arvind@DESKTOP-4NONCLR>
2021-04-06 22:36:50 -0700

    commited by arvind Ass-1_PS-5

---
+++ Assigment-1-Problem-Statement5/src/com/arvind/realcoderz/Tester.java
@@ -0,0 +1,12 @@
+package com.arvind.realcoderz;
+
+public class Tester {
+	public static void main(String[] args) {
+		int x=0,y=0;
+		System.out.println(x+" "+y);
+		x=5+y++;
+		System.out.println(x+" "+y);
+		
+		
+	}
+}
