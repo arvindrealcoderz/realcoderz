8c7c3bc3a8d616ed7e69a88d3bc1f1f7a5e6873a
arvindtl <Arvind@DESKTOP-4NONCLR>
2021-04-06 20:32:51 -0700

    commit by arvind Assigment1_Problem16

---
+++ Assigment-1-Problem-Statement16/src/com/arvind/realcoderz/Tester.java
@@ -0,0 +1,18 @@
+package com.arvind.realcoderz;
+
+public class Tester {
+	public static void main(String[] args) {
+		int j=10;
+		switch (1) {
+		case 20:j+=1;
+		break;
+		case 40:j+=2;
+		break;
+
+		default:j+=3;
+			break;
+		case 1:j+=4;
+		System.out.println("hello");
+		}
+	}
+}
