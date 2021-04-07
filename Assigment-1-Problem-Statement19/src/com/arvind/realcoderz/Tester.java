4976d7a0e5a11a306b66b18250569d89c3f7234a
arvindtl <Arvind@DESKTOP-4NONCLR>
2021-04-06 22:20:53 -0700

    Commited by arvind A-1_PS_20

---
+++ Assigment-1-Problem-Statement19/src/com/arvind/realcoderz/Tester.java
@@ -0,0 +1,12 @@
+package com.arvind.realcoderz;
+
+public class Tester {
+	
+	public static void main(String[] args) {
+		String entries[] = {"entries","entries"};
+		int count=0;
+		while (entries[count++]!=null) {
+			System.out.println(count);
+		}
+	}
+}
