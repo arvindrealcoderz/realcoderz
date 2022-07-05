33aa602408eb7127ea65408deb62acde63d81955
Arvindv7238 <91256240+Arvindv7238@users.noreply.github.com>
2022-07-05 06:53:23 +0530

    Update Tester.java
    change

--- Assigment-1-Problem-Statement-4/src/com/arvind/realcoderz/Tester.java
+++ Assigment-1-Problem-Statement-4/src/com/arvind/realcoderz/Tester.java
@@ -3,6 +3,7 @@ package com.arvind.realcoderz;
 public class Tester {
 	public static void main(String[] args) {
 		boolean flag=true;
+		
 		int x=5;
 		int result=(8*2)%x;
 		System.out.println(!(result>0)^true^(flag=false));
